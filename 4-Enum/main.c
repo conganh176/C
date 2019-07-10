adedcc56786cf677fc0963f9b43d73b1aa79faa0
Cong Anh Nguyen <sworlds@Congs-MacBook-Air.local>
2019-07-10 22:59:33 +0700

    .

---
+++ 4-Enum/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    enum Company { GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT };
+    enum Company xerox = XEROX;
+    enum Company google = GOOGLE;
+    enum Company ebay = EBAY;
+
+    printf("Xerox: %d\n", xerox);
+    printf("Google: %d\n", google);
+    printf("Ebay: %d\n", ebay);
+
+    return 0;
+}
