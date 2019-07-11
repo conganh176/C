fbf38df47b69374b0cffd1eb18ba560de7a44db1
Cong Anh Nguyen <sworlds@Congs-MacBook-Air.local>
2019-07-11 13:00:11 +0700

    .

---
+++ 6-DisplayByteSize/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nSize of int: %li", sizeof(int));
+    printf("\nSize of char: %li", sizeof(char));
+    printf("\nSize of long: %li", sizeof(long));
+    printf("\nSize of long long: %li", sizeof(long long));
+    printf("\nSize of double: %li", sizeof(double));
+    printf("\nSize of long double: %li", sizeof(long double));
+
+    return 0;
+}
